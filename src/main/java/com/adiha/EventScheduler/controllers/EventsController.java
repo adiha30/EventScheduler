69e62abfba6c992eb4fe18065e62f6d73d1a186b
adiharel <adi.harel10@gmail.com>
2023-12-07 08:37:39 +0200

    add filters

--- src/main/java/com/adiha/EventScheduler/controllers/EventsController.java
+++ src/main/java/com/adiha/EventScheduler/controllers/EventsController.java
@@ -2,10 +2,12 @@ package com.adiha.EventScheduler.controllers;
 
 import com.adiha.EventScheduler.models.Event;
 import com.adiha.EventScheduler.repositories.EventRepository;
+import com.adiha.EventScheduler.specifications.EventByLocation;
 import com.adiha.EventScheduler.utils.mapper.EventMapper;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +25,7 @@ public class EventsController {
     private final EventRepository eventRepository;
     private final EventMapper eventMapper;
 
+    @ResponseStatus(HttpStatus.FOUND)
     @GetMapping("/events")
     public List<Event> getAllEvents() {
         logger.debug("Retrieving all events");
@@ -30,6 +33,7 @@ public class EventsController {
         return eventRepository.findAll();
     }
 
+    @ResponseStatus(HttpStatus.FOUND)
     @GetMapping("/events/{eventId}")
     public Event getEventById(@PathVariable(value = "eventId") UUID eventId) {
         logger.debug("Retrieving event with id: {}", eventId);
@@ -37,6 +41,21 @@ public class EventsController {
         return eventRepository.findById(eventId).orElseThrow(() -> throwNotFoundException(eventId));
     }
 
+    @ResponseStatus(HttpStatus.FOUND)
+    @GetMapping("/events/filter")
+    public List<Event> getEventsByLocationAndVenue(
+            @RequestParam(value = "location", required = false) String location,
+            @RequestParam(value = "venue", required = false) String venue) {
+        logger.debug("Retrieving events with location: {} and venue: {}", location, venue);
+
+        Specification<Event> spec = Specification
+                .where(new EventByLocation(location))
+                .and(new EventByLocation(venue));
+
+        return eventRepository.findAll(spec);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/events")
     public Event createEvent(@Validated @RequestBody Event event) {
         logger.debug("Creating event: {}", event);
@@ -44,6 +63,7 @@ public class EventsController {
         return eventRepository.save(event);
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @PatchMapping("/events/{eventId}")
     public Event updateEvent(@PathVariable(value = "eventId") UUID eventId, @Validated @RequestBody Event event) {
         logger.debug("Updating event with id: {}", eventId);
@@ -55,6 +75,7 @@ public class EventsController {
         return eventRepository.save(updatedEvent);
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @DeleteMapping("/events/{eventId}")
     public Event deleteEvent(@PathVariable(value = "eventId") UUID eventId) {
         logger.debug("Deleting event with id: {}", eventId);
@@ -63,7 +84,7 @@ public class EventsController {
                 .map(this::deleteAndReturn)
                 .orElseThrow(() -> throwNotFoundException(eventId));
     }
-    
+
     private static ResponseStatusException throwNotFoundException(UUID eventId) {
         return new ResponseStatusException(
                 HttpStatus.NOT_FOUND,
--- src/main/java/com/adiha/EventScheduler/repositories/EventRepository.java
+++ src/main/java/com/adiha/EventScheduler/repositories/EventRepository.java
@@ -2,8 +2,9 @@ package com.adiha.EventScheduler.repositories;
 
 import com.adiha.EventScheduler.models.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface EventRepository extends JpaRepository<Event, UUID> {
+public interface EventRepository extends JpaRepository<Event, UUID>, JpaSpecificationExecutor<Event> {
 }
